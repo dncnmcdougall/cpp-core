d823c6c2bec33fae1a589fa986683e733ac7b61c
Duncan McDougall <dncn.mcdougall@gmail.com>
2022-02-06 17:35:58 +0000

    Adds install target, improves logging api

--- src/logging.cpp
+++ src/logging.cpp
@@ -3,10 +3,4 @@
 namespace logging
 {
 
-Logging error(Logging::ERROR);
-Logging warn(Logging::WARNING);
-Logging message(Logging::MESSAGE);
-Logging debug(Logging::DEBUG);
-
-Logging::Level Logging::s_level = MESSAGE;
 }
--- src/logging.h
+++ src/logging.h
@@ -1,74 +1,82 @@
 #pragma once
+
 #include <iostream>
 
 namespace logging
 {
-
-class Logging
-{
-public:
-
-    enum Level {
-    ERROR=0,
-    WARNING=1,
-    MESSAGE=2,
-    DEBUG=3
-    };
-
-    Logging(Level level) : m_level(level) {};
-
-    typedef std::basic_ostream<char, std::char_traits<char>> CoutType;
-    Logging& operator<<(CoutType& (*value)(CoutType&) ) { 
-        if ( m_level <= s_level) {
-            switch(m_level) {
-            case ERROR: [[fallthrough]];
-            case DEBUG:
-                std::cerr<<value;
-                break;
-            case MESSAGE: [[fallthrough]];
-            case WARNING:
-                std::cout<<value;
-                break;
-            default:
-                std::cout<<value;
-                break;
-            }
-        }
-        return *this;
-    };
-
-    template<typename T>
-    Logging& operator<<(const T& value) { 
-        if ( m_level <= s_level) {
-            switch(m_level) {
-            case ERROR: [[fallthrough]];
-            case DEBUG:
-                std::cerr<<value;
-                break;
-            case MESSAGE: [[fallthrough]];
-            case WARNING:
-                std::cout<<value;
-                break;
-            default:
-                std::cout<<value;
-                break;
-            }
-        }
-        return *this;
-    }
-
-    static void setLevel(Level level) { s_level=level; };
-
-private:
-    const Level m_level;
-
-    static Level s_level;
-
+enum class Level {
+    Error=0,
+    Warning=1,
+    Message=2,
+    Debug=3
 };
 
-extern Logging error;
-extern Logging warn;
-extern Logging message;
-extern Logging debug;
+
+class Logger
+    {
+        public:
+
+        Logger(Level level) : m_level(level) {};
+
+        typedef std::basic_ostream<char, std::char_traits<char>> CoutType;
+        Logger& operator<<(CoutType& (*value)(CoutType&) ) { 
+            if ( m_level <= s_level) {
+                switch(m_level) {
+                    case Level::Error: [[fallthrough]];
+                    case Level::Debug:
+                        std::cerr<<value;
+                        break;
+                    case Level::Message: [[fallthrough]];
+                    case Level::Warning:
+                        std::cout<<value;
+                        break;
+                    default:
+                        std::cout<<value;
+                        break;
+                }
+            }
+            return *this;
+        };
+
+        template<typename T>
+        Logger& operator<<(const T& value) { 
+            if ( m_level <= s_level) {
+                switch(m_level) {
+                    case Level::Error: [[fallthrough]];
+                    case Level::Debug:
+                        std::cerr<<value;
+                        break;
+                    case Level::Message: [[fallthrough]];
+                    case Level::Warning:
+                        std::cout<<value;
+                        break;
+                    default:
+                        std::cout<<value;
+                        break;
+                }
+            }
+            return *this;
+        }
+
+        friend void setLevel(Level level);
+
+        private:
+        const Level m_level;
+
+        static Level s_level;
+
+    };
+
+void setLevel(Level level) 
+{ 
+    Logger::s_level=level; 
+};
+
+Logger error(Level::Error);
+Logger warn(Level::Warning);
+Logger message(Level::Message);
+Logger debug(Level::Debug);
+
+Level Logger::s_level = Level::Message;
 
 };
